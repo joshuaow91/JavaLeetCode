054b3e9d46b36435e3d8b91338ed888042febdd1
Joshua Ow <joshuaow@gmail.com>
2023-10-20 11:41:46 -0500

    linked lists

---
+++ src/MiddleNodeLinkedList.java
@@ -0,0 +1,43 @@
+public class MiddleNodeLinkedList {
+
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null) {
+                slow = slow.next; // Move by 1 step
+                fast = fast.next.next; // Move by 2 steps
+            }
+
+            return slow; // Return the middle node
+        }
+    }
+
+      public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+}
+
+//Given the head of a singly linked list, return the middle node of the linked list.
+//
+//If there are two middle nodes, return the second middle node.
+//
+//
+//
+//Example 1:
+//
+//
+//Input: head = [1,2,3,4,5]
+//Output: [3,4,5]
+//Explanation: The middle node of the list is node 3.
+//Example 2:
+//
+//
+//Input: head = [1,2,3,4,5,6]
+//Output: [4,5,6]
+//Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
