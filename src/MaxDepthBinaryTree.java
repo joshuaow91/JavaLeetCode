a6091d29bfb712a95e17261417505d3be07ecd18
Joshua Ow <joshuaow@gmail.com>
2023-10-22 14:31:26 -0500

    java leetcode

--- src/MaxDepthBinaryTree.java
+++ src/MaxDepthBinaryTree.java
@@ -1,34 +1,39 @@
-import javax.swing.tree.TreeNode;
-
 public class MaxDepthBinaryTree {
     public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
 
-        TreeNode temp = root;
-        int leftCount = 0;
-        int rightCount = 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
 
-        while (temp != null) {
-            temp = temp.left;
-            leftCount++;
-            if(temp.right != null) {
-                temp = temp.right;
-                rightCount++;
-            }
-        }
-
-        return Math.max(leftCount, rightCount);
+        return 1 + Math.max(left, right);
     }
 
-     * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+      public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+     TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
 }
+
+//Given the root of a binary tree, return its maximum depth.
+//
+//A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+//
+//
+//
+//Example 1:
+//
+//
+//Input: root = [3,9,20,null,null,15,7]
+//Output: 3
+//Example 2:
+//
+//Input: root = [1,null,2]
+//Output: 2
