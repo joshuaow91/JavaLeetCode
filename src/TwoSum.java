79af51c18df057a0be75b7cd59c71a846b8be6f4
Joshua Ow <joshuaow@gmail.com>
2023-12-02 13:05:12 -0600

    hash map solution to TwoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,22 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
 //    Brute Force Solution
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++){
-                if (nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                }
-            }
-        }
-        return result;
-    }
+//    public int[] twoSum(int[] nums, int target) {
+//        int[] result = new int[2];
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++){
+//                if (nums[i] + nums[j] == target) {
+//                    result[0] = i;
+//                    result[1] = j;
+//                }
+//            }
+//        }
+//        return result;
+//    }
 
 //    HashMap Solution
+public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
 
+    for(int i = 0; i < nums.length; i++) {
+// check if the complement of a previously encountered number is present in the current iteration
+        Integer complementIndex = map.get(nums[i]);
+// if the number is not null a match has been found and the indices are returned in a new int array
+        if (complementIndex != null) {
+            return new int[]{i, complementIndex};
+        }
+// if the value is null, it means the current number's complement hasn't been seen yet.
+// calculate this complement (target - nums[i]) and store it in the map along with the current index.
+// this entry will be used in future iterations to check if any upcoming number is the complement of the current number.
+        map.put(target - nums[i], i);
+    }
+    return nums;
+}
 
 }
 
