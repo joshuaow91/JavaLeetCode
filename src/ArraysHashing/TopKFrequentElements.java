5a0854541003e146508a3fea2737bdccd6b32617
Joshua Ow <joshuaow@gmail.com>
2023-12-08 17:41:30 -0600

    java leet

--- src/ArraysHashing/TopKFrequentElements.java
+++ src/ArraysHashing/TopKFrequentElements.java
@@ -1,4 +1,24 @@
 package ArraysHashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TopKFrequentElements {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+
+        for (int i = 0; i < nums.length; i++){
+            Integer num = map.get(i);
+            if (num != null) {
+                map.put(i, k + 1);
+            }
+            return new int[]{i, k};
+        }
+        return nums;
+    }
 }
+
+
+//incomplete. working through this.
