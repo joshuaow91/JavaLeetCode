aefe1fd49b4d2a0f87d35bf2bab028cc734d49c2
Joshua Ow <joshuaow@gmail.com>
2023-12-31 16:58:37 -0600

    java leet

--- src/SearchInsertPosition.java
+++ src/SearchInsertPosition.java
@@ -18,6 +18,27 @@ public class SearchInsertPosition {
 
             return left;
         }
+
+
+//        public int searchInsert(int[] nums, int target) {
+//    return binarySearch(nums, target, 0, nums.length - 1);
+//}
+//
+//private int binarySearch(int[] nums, int target, int left, int right) {
+//    if (left > right) {
+//        return left;
+//    }
+//
+//    int mid = left + (right - left) / 2;
+//
+//    if (nums[mid] == target) {
+//        return mid;
+//    } else if (nums[mid] < target) {
+//        return binarySearch(nums, target, mid + 1, right);
+//    } else {
+//        return binarySearch(nums, target, left, mid - 1);
+//    }
+//}
 }
 
 
