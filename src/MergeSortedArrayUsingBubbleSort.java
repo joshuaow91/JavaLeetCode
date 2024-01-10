08672aa735774c169e03a4478d3bd7dccd3638d0
Joshua Ow <joshuaow@gmail.com>
2024-01-10 17:16:49 -0600

    java leet, used a bubble sort on a merge sort problem for fun

--- src/MergeSortedArrayUsingBubbleSort.java
+++ src/MergeSortedArrayUsingBubbleSort.java
@@ -1,36 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MergeSortedArrayUsingBubbleSort {
 
+//    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//        int nums1Pointer = m - 1;
+//        int nums2Pointer = n - 1;
+//        int capacityPointer = m + n - 1;
+//
+//        int iterationCounter = 0;
+//
+//        while (nums1Pointer >= 0 && nums2Pointer >= 0) {
+//            if (nums1[nums1Pointer] > nums2[nums2Pointer]) {
+//                nums1[capacityPointer] = nums1[nums1Pointer];
+//                nums1Pointer--;
+//            } else {
+//                nums1[capacityPointer] = nums2[nums2Pointer];
+//                nums2Pointer--;
+//            }
+//            capacityPointer--;
+//
+//            iterationCounter++;
+//            System.out.println("Iteration " + iterationCounter + ": " + arrayToString(nums1, m + n));
+//        }
+//
+//        int iterationCounter2 = 0;
+//
+//        while (nums2Pointer >= 0) {
+//            nums1[capacityPointer] = nums2[nums2Pointer];
+//            nums2Pointer--;
+//            capacityPointer--;
+//
+//            iterationCounter2++;
+//            System.out.println("Left over Elements Iteration: " + iterationCounter2 + " " + arrayToString(nums1, m + n));
+//        }
+//    }
+
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int nums1Pointer = m - 1;
-        int nums2Pointer = n - 1;
-        int capacityPointer = m + n - 1;
+        List<Integer> list = new ArrayList<>();
 
-        int iterationCounter = 0;
-
-        while (nums1Pointer >= 0 && nums2Pointer >= 0) {
-            if (nums1[nums1Pointer] > nums2[nums2Pointer]) {
-                nums1[capacityPointer] = nums1[nums1Pointer];
-                nums1Pointer--;
-            } else {
-                nums1[capacityPointer] = nums2[nums2Pointer];
-                nums2Pointer--;
+        for (int i : nums1) {
+            if (i > 0) {
+                list.add(i);
             }
-            capacityPointer--;
-
-            iterationCounter++;
-            System.out.println("Iteration " + iterationCounter + ": " + arrayToString(nums1, m + n));
         }
 
-        int iterationCounter2 = 0;
-
-        while (nums2Pointer >= 0) {
-            nums1[capacityPointer] = nums2[nums2Pointer];
-            nums2Pointer--;
-            capacityPointer--;
-
-            iterationCounter2++;
-            System.out.println("Left over Elements Iteration: " + iterationCounter2 + " " + arrayToString(nums1, m + n));
+        for (int j : nums2) {
+            if (j > 0) {
+                list.add(j);
+            }
         }
+
+        int temp = 0;
+
+        for (int k = 0; k < list.size() - k; k++) {
+            for (int l = 0; l < list.size() - k - 1; l++) {
+                if (list.get(l) > list.get(l + 1)) {
+                    temp = list.get(l);
+                    list.set(l, list.get(l + 1));
+                    list.set(l + 1, temp);
+                }
+            }
+        }
+
+        System.out.println("Bubble sort, with Array List: " + list);
     }
 
     public static void main(String[] args) {
@@ -39,14 +72,14 @@ public class MergeSortedArrayUsingBubbleSort {
         int m = 3;
         int n = nums2.length;
 
-        System.out.println("Before merge:");
-        System.out.println("nums1: " + arrayToString(nums1, m + n));
-        System.out.println("nums2: " + arrayToString(nums2, n));
+//        System.out.println("Before merge:");
+//        System.out.println("nums1: " + arrayToString(nums1, m + n));
+//        System.out.println("nums2: " + arrayToString(nums2, n));
 
         merge(nums1, m, nums2, n);
 
-        System.out.println("After merge:");
-        System.out.println("nums1: " + arrayToString(nums1, m + n));
+//        System.out.println("After merge:");
+//        System.out.println("nums1: " + arrayToString(nums1, m + n));
     }
 
     private static String arrayToString(int[] array, int length) {
