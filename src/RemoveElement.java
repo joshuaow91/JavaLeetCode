666a43d0f1dab88babf11b682ccfdb929f5c6d62
Joshua Ow <joshuaow@gmail.com>
2024-01-12 10:19:13 -0600

    java leet

--- src/RemoveElement.java
+++ src/RemoveElement.java
@@ -3,9 +3,8 @@ public class RemoveElement {
         int count = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == val) {
-                char c = (char) nums[i];
-            } else {
+            if (nums[i] != val) {
+                nums[count] = nums[i];
                 count++;
             }
         }
