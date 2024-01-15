108c1d312b275d1131334319c2eb544bf2efacf5
Joshua Ow <joshuaow@gmail.com>
2024-01-15 17:20:16 -0600

    java leet

---
+++ src/ArraysHashing/MostFrequentEvenElement.java
@@ -0,0 +1,11 @@
+package ArraysHashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MostFrequentEvenElement {
+    public int mostFrequentEven(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+    }
+}
