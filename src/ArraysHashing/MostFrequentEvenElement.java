f052bfe2f90f6a7d208c9036425da5ddec127027
Joshua Ow <joshuaow@gmail.com>
2024-01-16 18:18:07 -0600

    leet java

--- src/ArraysHashing/MostFrequentEvenElement.java
+++ src/ArraysHashing/MostFrequentEvenElement.java
@@ -4,8 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MostFrequentEvenElement {
-    public int mostFrequentEven(int[] nums) {
+    public static void mostFrequentEven(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
+
+        int count = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                map.put(num, count);
+                count++;
+            }
+        }
+        System.out.println(map);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 2, 4, 4, 1};
+        mostFrequentEven(nums);
 
     }
 }
